153ca61cc5aac524723416c65d8ee335ff32aa64
Diego Valencia <a01784568@tec.mx>
2024-08-27 17:24:18 -0600

    Created backend and schema

---
+++ database/schema.sql
@@ -0,0 +1,10 @@
+CREATE database crmSanders;
+USE crmSanders;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
