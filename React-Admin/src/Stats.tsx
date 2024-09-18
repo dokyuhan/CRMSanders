20ab278400983a2b4cfe3c29ca437080751670fe
Diego Valencia <a01784568@tec.mx>
2024-09-17 21:35:50 -0600

    Updated stats

--- React-Admin/src/Stats.tsx
+++ React-Admin/src/Stats.tsx
@@ -79,22 +79,3 @@ export const Stats = () => {
         </Card>
     );
 };
-export const Stats = () => (
-    <Card>
-        <CardHeader title="Statistics" />
-        <CardContent>
-            <List>
-                <ListItem>
-                    <ListItemText primary="Total Users" secondary="1,250" />
-                </ListItem>
-                <ListItem>
-                    <ListItemText primary="Active Projects" secondary="300" />
-                </ListItem>
-                <ListItem>
-                    <ListItemText primary="Revenue" secondary="$1,200,000" />
-                </ListItem>
-            </List>
-        </CardContent>
-    </Card>
-);
-
