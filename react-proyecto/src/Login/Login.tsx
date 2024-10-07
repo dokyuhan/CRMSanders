844b371c4e38a8c3c2effc09215a32122da2f23f
EnriqueMartinezdeVR <142240552+EnriqueMartinezdeVR@users.noreply.github.com>
2024-10-06 18:13:28 -0600

    Estilo de log in modificado

--- react-proyecto/src/Login/Login.tsx
+++ react-proyecto/src/Login/Login.tsx
@@ -19,13 +19,18 @@ const LoginPage: React.FC = () => {
 
   return (
     <>
-      <div className="min-h-screen bg-gradient-to-r from-blue-600 to-blue-800 flex items-center justify-center">
+      <div
+        className="min-h-screen flex items-center justify-center"
+        style={{
+          background: 'linear-gradient(135deg, #f0f0f0 50%, #c4c4c4 50%)', 
+        }}
+      >
         <div className="bg-white bg-opacity-80 p-10 rounded-lg shadow-md w-full max-w-4xl flex">
           <div className="flex-1 p-4">
-            <img 
+            <img
               className="w-full max-w-xs rounded-lg"
-              src="/public/Logo_sanders.jpeg" 
-              alt="Logo Fundación Sanders" 
+              src="/public/Logo_sanders.jpeg"
+              alt="Logo Fundación Sanders"
             />
           </div>
           <div className="flex-1 flex flex-col mx-4 justify-center">
@@ -33,16 +38,16 @@ const LoginPage: React.FC = () => {
               <input
                 className="bg-white bg-opacity-80 shadow-md w-full p-4 mb-4 border-b-2 border-gray-400 text-gray-700 focus:border-blue-600 focus:outline-none transition-colors duration-300"
                 value={username}
-                onChange={e => setUsername(e.target.value)}
-                placeholder="Username"
+                onChange={(e) => setUsername(e.target.value)}
+                placeholder="Usuario"
                 type="text"
                 required
               />
               <input
                 className="bg-white bg-opacity-80 shadow-md w-full p-4 mb-4 border-b-2 border-gray-400 text-gray-700 focus:border-blue-600 focus:outline-none transition-colors duration-300"
                 value={password}
-                onChange={e => setPassword(e.target.value)}
-                placeholder="Password"
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="Contraseña"
                 type="password"
                 required
               />
@@ -50,11 +55,11 @@ const LoginPage: React.FC = () => {
                 type="submit"
                 className="w-full py-3 bg-blue-500 text-white rounded-md hover:bg-blue-600 transition-transform transform hover:translate-y-0.5 duration-300"
               >
-                Log In
+                Iniciar sesión
               </button>
             </form>
             <p className="mt-4 text-center text-gray-700 text-sm">
-              No tienes una cuenta?{' '}
+              ¿No tienes una cuenta?{' '}
               <Link to="/register" className="font-semibold text-gray-900 hover:underline">
                 Regístrate
               </Link>
