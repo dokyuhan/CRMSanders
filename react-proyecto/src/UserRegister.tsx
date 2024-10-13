6ce52119d394c41d5bc47cc53fb261467e2da6ee
Diego Valencia <a01784568@tec.mx>
2024-10-12 20:20:06 -0600

    Changed fetch to data provider

--- react-proyecto/src/UserRegister.tsx
+++ react-proyecto/src/UserRegister.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { useNotify } from 'react-admin';
 import { Link } from 'react-router-dom';
+import { dataProvider } from './dataProvider';
+
 const Registercolab: React.FC = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
@@ -8,27 +10,23 @@ const Registercolab: React.FC = () => {
   const [role, setRole] = useState('');
   const [registrationSuccess, setRegistrationSuccess] = useState(false);
   const notify = useNotify();
+
   const handleSubmit = async (event: React.FormEvent) => {
     event.preventDefault();
     try {
-      const response = await fetch('https://localhost:3003/registercolab', { 
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ username, password, email, role }),
+      const response = await dataProvider.create('registercolab', {
+        data: { username, password, email, role },
       });
-      const data = await response.json();
-      if (response.ok) {
+      
+      if (response) {
         setRegistrationSuccess(true);
         notify('Registro exitoso');
-      } else {
-        notify(data.msg || 'Error en el registro');
       }
     } catch (error) {
       notify('Error en el registro');
     }
   };
+
   return (
     <div className='flex flex-col md:flex-row gap-8 m-4'>
       <div className='flex-1'>
@@ -66,7 +64,7 @@ const Registercolab: React.FC = () => {
               type="email"
               required
             />
-             <select
+            <select
               className="w-full p-4 mb-4 border-b-2 border-gray-400 text-gray-700 bg-white bg-opacity-80 shadow-md focus:border-blue-600 focus:outline-none transition-colors duration-300"
               value={role}
               onChange={e => setRole(e.target.value)}
@@ -86,8 +84,8 @@ const Registercolab: React.FC = () => {
           </div>
         </form>
       </div>
-      
     </div>
   );
 };
+
 export default Registercolab;
